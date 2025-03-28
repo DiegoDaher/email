c778489c58ec12dfe2bead1e38c38482a6378010
Diego Daher Diaz Contreras <169078085+DiegoDaher@users.noreply.github.com>
2025-03-27 23:56:49 -0600

    Update rabbitServiceListener.js

--- src/services/rabbitServiceListener.js
+++ src/services/rabbitServiceListener.js
@@ -4,7 +4,7 @@ import transporter from '../config/emailConfig.js';
 
 dotenv.config();
 
-const RABBITMQ_URL = "amqps://znlzcnga:fdx2PN2SUt1ofLrpMYEGIKew8IuvSXsW@possum.lmq.cloudamqp.com/znlzcnga";
+const RABBITMQ_URL = "amqps://sbftbpns:PX-K11id-bbob1ef-3_Xhb8vgBEC-k9d@woodpecker.rmq.cloudamqp.com/sbftbpns";
 
 
 export async function userEvents() {
